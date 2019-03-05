00bab9e948bc39648bcee4448c6d6210d8cae4d8
Cristian Felipe Gallego Ramirez <Cgallego@intergrupo.com>
2019-03-05 08:43:10 -0500

    Unit test for bussiness layer codification.

--- HumanResources.Mvc.Tests/UnitTest1.cs
+++ HumanResources.Mvc.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using HumanResources.Business;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace HumanResources.Mvc.Tests
@@ -7,8 +8,24 @@ namespace HumanResources.Mvc.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void MonthlyContractTypeAnnualSalaryCalculationWorks()
         {
+            SalaryBusiness salaryBusiness = new SalaryBusiness();
+            int montlySalary = 100;
+            decimal annualSalaryExpected = 1200;
+            decimal annualSalaryCalculated = salaryBusiness.CalculateMontlySalaryAnnualSalary(montlySalary);
+            Assert.AreEqual(annualSalaryExpected, annualSalaryCalculated);
+
+        }
+
+        [TestMethod]
+        public void HourlyContractTypeAnnualSalaryCalculationWorks()
+        {
+            SalaryBusiness salaryBusiness = new SalaryBusiness();
+            int hourlySalary = 100;
+            decimal annualSalaryExpected = 144000;
+            decimal annualSalaryCalculated = salaryBusiness.CalculateHourlySalaryAnnualSalary(hourlySalary);
+            Assert.AreEqual(annualSalaryExpected, annualSalaryCalculated);
         }
     }
 }

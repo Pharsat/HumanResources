c9cea34b58ba043f16322e88da5086b54140a210
Cristian Felipe Gallego Ramirez <Cgallego@intergrupo.com>
2019-03-05 08:33:08 -0500

    set propper newtonsoft json to web config.

--- HumanResources.Entities/Role.cs
+++ HumanResources.Entities/Role.cs
@@ -4,13 +4,10 @@
 
     public class Role
     {
-        [JsonProperty("roleId")]
         public int Id { get; set; }
 
-        [JsonProperty("roleName")]
         public string Name { get; set; }
 
-        [JsonProperty("roleDescription")]
         public string Description { get; set; }
     }
 }
